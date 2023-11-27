a229048fa1bad93d4d689d3c0ea03e82dfcaf287
OlegatorLE <oleh.travin@gmail.com>
2023-11-27 12:08:25 +0200

    Initialize django project & create models

---
+++ web/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    url = models.URLField()
+    technologies = models.ManyToManyField('Technology', related_name='jobs')
+
+
+class Technology(models.Model):
+    name = models.CharField(max_length=100)
