b1d27b4d59fde0026ce0306e1bbb8676a8639527
OlegatorLE <oleh.travin@gmail.com>
2024-01-20 23:22:58 +0200

    Changed hosts

--- python_technologies_statistics/settings.py
+++ python_technologies_statistics/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["18.215.237.190"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
